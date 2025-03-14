85812c8070b1be942144be173c93e1efcf27b342
websurf500 <websurf500@gmail.com>
2025-03-14 12:22:10 +0530

    backend_update_constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a34c861ab9bd542ea98514895512019f-14351403.ap-south-1.elb.amazonaws.com',
 };
